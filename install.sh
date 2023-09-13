91613aa9553034d88e07e5291a40cc6ee1bf0999
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2023-09-13 11:30:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 config_dir=~/printer_data/config
 
-ln -sfn ./{printer.cfg,ender3-v2} $config_dir
+ln -sfn ./{printer.cfg,ender3-v2.cfg} $config_dir
 cd $config_dir && mkdir -pv macros
 cd -
 ln -sfn ./macros/print.cfg $config_dir/macros
